06e9d2d747ec3d965aebc2e794f9e0662faaa7cd
VoidFrog1 <samj09209@gmail.com>
2024-02-10 20:39:59 -0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,39 +4,6 @@ const app = express();
 
 
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -54,7 +21,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3030;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`usefull programmer for info sec ${port}`);
 });
